68d56f0e7165c0cf5bdf0f5b88dfee972ff954b3
Ivan Koleda <ivnkld@gmail.com>
2020-07-12 21:22:49 +0300

    change db on postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,21 +5,27 @@ import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 import { UsersModule } from './users/users.module';
 
+const getDbConnectionConfig = () =>
+  process.env.DATABASE_URL
+    ? { url: process.env.DATABASE_URL }
+    : {
+        host: 'localhost',
+        port: 5432,
+        username: 'root',
+        password: 'root',
+        database: 'root',
+      };
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
     // TODO check if it is possible to get settings from ormconfig
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3307,
-      username: 'root',
-      password: 'root',
-      database: 'shop',
+      type: 'postgres',
       autoLoadEntities: true,
       synchronize: true,
       keepConnectionAlive: true,
-      // dropSchema: true,
+      ...getDbConnectionConfig(),
     }),
     ProductsModule,
     OrdersModule,
